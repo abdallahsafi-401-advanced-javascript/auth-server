c2508f4f74c6e12127a64cfd18462a9b67e8808e
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-09-22 22:42:46 +0300

    update readme

--- __test__/ auth.router.test.js
+++ __test__/ auth.router.test.js
@@ -23,33 +23,43 @@ describe('Auth Router', () => {
       expect(token).toBeDefined();
     });
   });
-  it('can get() all users ', async () => {
+
+
+  it('can get() all users with bearer token', async () => {
     const user1 = { username: 'ahmed', password: '1234' };
     const user2 = { username: 'samy', password: '1234' };
     const userList = [user1, user2];
     await mockRequest.post('/users').send(user1);
     await mockRequest.post('/users').send(user2);
-    const usersRet = await mockRequest.get('/users');
+    const results = await mockRequest.post('/signin').auth('waleed', '1234');
+    const usersRet = await mockRequest
+      .get('/users')
+      .set('Authorization', `Bearer ${results.body.token}`);
     const userItem = usersRet.body.results;
     userItem.forEach((key) => {
       expect(userItem[key]).toEqual(userList[key]);
     });
   });
 
-  it('should respond with 404 for not found routes', ()=>{
-    return mockRequest.get('/users/notFound').then(result=>{
-      expect(result.status).toBe(404);
-    }).catch(err=> {
-      console.log(err);
-    });
+  it('should respond with 404 for not found routes', () => {
+    return mockRequest
+      .get('/users/notFound')
+      .then((result) => {
+        expect(result.status).toBe(404);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
-  it('should respond with 500 for bad routes', ()=>{
-    return mockRequest.get('/bad').then(result=>{
-      expect(result.status).toBe(500);
-    }).catch(err=> {
-      console.log(err);
-    });
+  it('should respond with 500 for bad routes', () => {
+    return mockRequest
+      .get('/bad')
+      .then((result) => {
+        expect(result.status).toBe(500);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
-
 });
--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -10,24 +10,16 @@ const users = require('../models/users/users-model.js');
  * @param {*} next 
  */
 module.exports = (req, res, next) => {
-  // req.headers.authorization should be : "Basic sdkjdsljd="
   if (!req.headers.authorization) {
     next('Invalid Login');
     return;
   }
-
-  // Pull out just the encoded part by splitting the header into an array on the space and popping off the 2nd element
   let basic = req.headers.authorization.split(' ').pop();
-
-
-  // decodes to user:pass and splits it to an array
   let [user, pass] = base64.decode(basic).split(':');
   let userRecord = {
     username: user,
     password: pass,
   };
-
-  // Is this user ok?
   users
     .authenticateBasic(userRecord)
     .then((validUser) => {
